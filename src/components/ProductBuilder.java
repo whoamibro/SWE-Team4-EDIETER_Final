f1fbba8126653223fd7c5ca5660dac2b470b1c0d
geronimo124 <geronimo124@hanmail.net>
2016-11-22 21:04:24 +0900

    Product builder class 추가

---
+++ src/components/ProductBuilder.java
@@ -0,0 +1,64 @@
+package components;
+
+public class ProductBuilder {
+    private String type;
+    private String model;
+    private String nickName;
+    private int usingTime;
+    private int availableTime;
+    private double power;
+    // 냉방전력, 난방전력
+    private double cool_heatpower;
+    private int grade;
+
+    public ProductBuilder setType(String type) {
+        this.type = type;
+        return this;
+    }
+
+    public ProductBuilder setModel(String model) {
+        this.model = model;
+        return this;
+    }
+
+    public ProductBuilder setNickName(String nickName) {
+        this.nickName = nickName;
+        return this;
+    }
+
+    public ProductBuilder setUsingTime(int usingTime) {
+        this.usingTime = usingTime;
+        return this;
+    }
+
+    public ProductBuilder setAvailableTime(int availableTime) {
+        this.availableTime = availableTime;
+        return this;
+    }
+
+    public ProductBuilder setPower(double power) {
+        this.power = power;
+        return this;
+    }
+
+    public ProductBuilder getGrade(int grade) {
+        this.grade = grade;
+        return this;
+    }
+
+    public ProductBuilder getCool_heatpower(double cool_heatpower) {
+        this.cool_heatpower = cool_heatpower;
+        return this;
+    }
+
+    public ProductBuilder setCool_heatpower(double cool_heatpower) {
+        this.cool_heatpower = cool_heatpower;
+        return this;
+    }
+
+    public Product build() {
+        Product product = new Product(type, model, nickName,
+                usingTime, availableTime, power, cool_heatpower, grade);
+        return product;
+    }
+}
