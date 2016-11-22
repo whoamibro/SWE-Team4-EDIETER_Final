5bedee8086ebae55347cc09933f9adf25a1738cf
geronimo124 <geronimo124@hanmail.net>
2016-11-22 20:54:30 +0900

    Product class 추가

---
+++ src/components/Product.java
@@ -0,0 +1,60 @@
+package components;
+
+public class Product {
+    private String type;
+    private String model;
+    private String nickName;
+    private int usingTime;
+    private int availableTime;
+    //추가 수정부분
+    private double cool_heatpower;
+    private double power;
+    private int grade;
+
+    public Product(String type, String model, String nickName, int usingTime, int availableTime, double power, double cool_heatpower, int grade) {
+        this.type = type;
+        this.model = model;
+        this.nickName = nickName;
+        this.usingTime = usingTime;
+        this.availableTime = availableTime;
+        this.power = power;
+        // 추가 수정부분
+        this.cool_heatpower = cool_heatpower;
+        this.grade = grade;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public String getNickName() {
+        return this.nickName;
+    }
+
+    public int getUsingTime() {
+        return this.usingTime;
+    }
+
+    public int getAvailableTime() {
+        return this.availableTime;
+    }
+
+    public double getPower() {
+        return this.power;
+    }
+
+    public double getCool_heatpower(){return this.cool_heatpower;}
+
+    public int getGrade() {
+        return this.grade;
+    }
+
+    public String getProductInfo() {
+        String productInfo = String.format("type:%s, model:%s, nickName:%s, usingTime:%d, availableTime:%d, power:%f, grade:%d", new Object[]{this.type, this.model, this.nickName, Integer.valueOf(this.usingTime), Integer.valueOf(this.availableTime), Double.valueOf(this.power), Integer.valueOf(this.grade)});
+        return productInfo;
+    }
+}
