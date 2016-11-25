c8bc5c5fc1872140fdda809acd71e1015e17b46f
SAMSUNG-PC\Samsung <cse2012@naver.com>
2016-11-26 04:28:59 +0900

    add hyperlink

--- src/controller/RegisterController.java
+++ src/controller/RegisterController.java
@@ -4,10 +4,14 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebEngine;
@@ -24,20 +28,8 @@ public class RegisterController implements Initializable {
 	@FXML
 	private AnchorPane paneRegister;
 
-	private Stage webStage;
-	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		WebView webView = new WebView();
-		WebEngine webEngine = webView.getEngine();
-		webEngine.load("http://cyber.kepco.co.kr/ckepco/front/jsp/CY/J/H/CYJHPP001.jsp");
-
-		webStage = new Stage();
-		Scene scene = new Scene(webView);
-		webStage.setWidth(1020);
-		webStage.setScene(scene);
-		webStage.setTitle("현재 전력 사용량 알기");
-		
 		btnOK.setOnAction(event -> {
 			btnOKHandler();
 		});
@@ -64,12 +56,33 @@ public class RegisterController implements Initializable {
 		try {
 			paneRegister.getChildren().add(loader.load());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void hyperlinkHandler() {
+		WebView webView = new WebView();
+		WebEngine webEngine = webView.getEngine();
+		webEngine.load("http://cyber.kepco.co.kr/ckepco/front/jsp/CY/J/H/CYJHPP001.jsp");
+
+		Stage webStage = new Stage();
+		Scene scene = new Scene(webView);
+		webStage.setWidth(1020);
+		webStage.setScene(scene);
+		webStage.setTitle("현재 전력 사용량 알기");
 		webStage.show();
+
+		webEngine.getLoadWorker().stateProperty().addListener(new ChangeListener<Worker.State>() {
+			@Override
+			public void changed(ObservableValue<? extends Worker.State> arg0, Worker.State arg1, Worker.State arg2) {
+				if (arg2 == Worker.State.FAILED) {
+					Alert alert = new Alert(Alert.AlertType.ERROR);
+					alert.setHeaderText("웹페이지 로드 오류");
+					alert.setContentText("인터넷 연결을 확인하세요!");
+					alert.showAndWait();
+					webStage.close();
+				}
+			}
+		});
 	}
 }
