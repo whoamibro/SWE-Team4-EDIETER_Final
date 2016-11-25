9394ed013db0f1fc0dce41b74af54e5ad1bfaed0
SAMSUNG-PC\Samsung <cse2012@naver.com>
2016-11-26 03:32:36 +0900

    add hyperlink

--- src/controller/RegisterController.java
+++ src/controller/RegisterController.java
@@ -7,8 +7,12 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
 
 public class RegisterController implements Initializable {
 	@FXML
@@ -20,8 +24,20 @@ public class RegisterController implements Initializable {
 	@FXML
 	private AnchorPane paneRegister;
 
+	private Stage webStage;
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		WebView webView = new WebView();
+		WebEngine webEngine = webView.getEngine();
+		webEngine.load("http://cyber.kepco.co.kr/ckepco/front/jsp/CY/J/H/CYJHPP001.jsp");
+
+		webStage = new Stage();
+		Scene scene = new Scene(webView);
+		webStage.setWidth(1020);
+		webStage.setScene(scene);
+		webStage.setTitle("현재 전력 사용량 알기");
+		
 		btnOK.setOnAction(event -> {
 			btnOKHandler();
 		});
@@ -51,5 +67,9 @@ public class RegisterController implements Initializable {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public void hyperlinkHandler() {
+		webStage.show();
 	}
 }
